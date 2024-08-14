624b0d8788981e237af06f95450df90638c149f1
Gavin的分身jp <gavin.zhaopf@foxmail.com>
2024-08-14 17:19:42 +0800

    开始做UDP同步练习

---
+++ Server/LearnUDP/Program.cs
@@ -0,0 +1,44 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+namespace LearnUDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.创建套接字
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //2.绑定本机地址
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+            socket.Bind(ipPoint);
+
+            //4.接受消息
+            byte[] bytes = new byte[512];
+            //这个变量主要是用来记录 谁发的信息给你 传入函数后 在内部 它会帮助我们进行赋值
+            EndPoint remoteIpPoint2 = new IPEndPoint(IPAddress.Any, 0);
+            int length = socket.ReceiveFrom(bytes, ref remoteIpPoint2);
+
+            Console.WriteLine("IP:" + (remoteIpPoint2 as IPEndPoint).Address.ToString() +
+                "port:" + (remoteIpPoint2 as IPEndPoint).Port +
+                "发来了" +
+                Encoding.UTF8.GetString(bytes, 0, length));
+
+
+            //3.发送到指定目标
+            //IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+            //指定要发送的字节数 和 远程计算机的 IP和端口
+            socket.SendTo(Encoding.UTF8.GetBytes("客户端发送消息"), remoteIpPoint2);
+
+
+            //5.释放关闭
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
