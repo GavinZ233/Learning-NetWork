a44ef047842686b7f84e991083fff9926361eb4a
Gavin的分身jp <gavin.zhaopf@foxmail.com>
2024-08-14 11:46:23 +0800

    自动生成课件工具完成，继续37课时

---
+++ Assets/Lesson/Lesson14_同步信息基础/Lesson14_同步信息基础.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+    
+public class Lesson14_同步信息基础 : MonoBehaviour
+{
+  
+
+}
+
--- Assets/Scripts/Tools/MyLessonTools.cs
+++ Assets/Scripts/Tools/MyLessonTools.cs
@@ -1,58 +1,81 @@
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using System;
 
 
 // Adds a component to the selected GameObjects
 
 class MyLessonTools : EditorWindow
 {
-    string[] options = { "Rigidbody", "Box Collider", "Sphere Collider" };
-    int index = 0;
-
+    string LessonName="LessonX_YYY";
+    public bool needOpenScene = true;
     [MenuItem("GavinTools/CreatLesson")]
     static void Init()
     {
         var window = GetWindow<MyLessonTools>();
-        window.position = new Rect(0, 0, 180, 80);
+        window.position = new Rect(500, 500, 300, 200);
         window.Show();
     }
 
     void OnGUI()
     {
-        index = EditorGUI.Popup(
-            new Rect(0, 0, position.width, 20),
-            "Component:",
-            index,
-            options);
+        GUI.Label(new Rect(0,0,100,30),"课件名称：") ;
+        LessonName= GUI.TextField(new Rect(120,0,150,30),LessonName);
+        needOpenScene = GUI.Toggle(new Rect(0,30,150,50),needOpenScene,"是否需要打开场景");
+        if (GUI.Button(new Rect(120, 30, 150, 30), "生成课件"))
+            CreatLesson();
+        if (GUI.Button(new Rect(120, 60, 150, 30), "挂脚本"))
+            AddScript();
 
-        if (GUI.Button(new Rect(0, 25, position.width, position.height - 26), "Add Component"))
-            AddComponentToObjects();
+
+
     }
 
-    void AddComponentToObjects()
+    void CreatLesson()
     {
-        if (!Selection.activeGameObject)
+        string path = Application.dataPath + "/Lesson/" + LessonName;
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        //创建脚本
+        string templatePath = System.IO.Path.Combine(UnityEditor.EditorApplication.applicationContentsPath, "Resources", "ScriptTemplates", "81-C# Script-NewBehaviourScript.cs.txt");
+        string scriptStr =File.ReadAllText(templatePath);
+        scriptStr = scriptStr.Replace("#ROOTNAMESPACEBEGIN#","");
+        scriptStr = scriptStr.Replace("#ROOTNAMESPACEEND#", "");
+        scriptStr = scriptStr.Replace("#SCRIPTNAME#", LessonName);
+        File.WriteAllText(path + "/" + LessonName + ".cs", scriptStr);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        //创建场景
+        Scene scene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects,NewSceneMode.Single);
+        EditorSceneManager.SaveScene(scene,path+"/"+LessonName+".unity");
+        AssetDatabase.Refresh();
+
+        if (!needOpenScene)
         {
-            Debug.LogError("Please select at least one GameObject first");
-            return;
+            EditorSceneManager.CloseScene(scene, true);
+
         }
 
-        foreach (GameObject obj in Selection.gameObjects)
-        {
-            switch (index)
-            {
-                case 0:
-                    obj.AddComponent<Rigidbody>();
-                    break;
+        AssetDatabase.Refresh();
 
-                case 1:
-                    obj.AddComponent<BoxCollider>();
-                    break;
+    }
 
-                case 2:
-                    obj.AddComponent<SphereCollider>();
-                    break;
-            }
-        }
+    void AddScript()
+    {
+        Camera ca = Camera.main;
+        ca.gameObject.AddComponent(Type.GetType(LessonName));
+        
+        CloseWindow();
+    }
+
+    void CloseWindow()
+    {
+        var window = GetWindow<MyLessonTools>();
+        window.Close();
+
     }
 }
